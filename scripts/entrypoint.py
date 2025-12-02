c6c9022df72aeb9d10a3c5f4a0a303025dfb919d
fuzzy-tribble <fancy-broccoli@pm.me>
2025-12-02 17:41:30 +0000

    updates

---
+++ scripts/entrypoint.py
@@ -0,0 +1,8 @@
+"""Main entrypoint for running DCS as a script.
+
+Todo:
+- dcs --help shows help
+- dcs run --game <game_name or path> --interface widget/cli/api run a game from a name or config file on a specified interface
+    + called run_api, run_widget, run_cli scripts based on interface selection
+- dcs deploy deploys a game to cloud providing you with a sharable link -> throw notimplemented error for now
+"""
--- scripts/main.py
+++
@@ -1,1 +0,0 @@
-# TODO: create a dcs script interface that takes --help, --deploy, --play (play local via cli, etc.)
