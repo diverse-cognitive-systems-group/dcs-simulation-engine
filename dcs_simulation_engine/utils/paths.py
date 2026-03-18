d9935f420e6cd227088f0fc8b277d8a69297611f
Eric Regina <8085239+ericreg@users.noreply.github.com>
2026-03-17 23:32:53 -0700

    fix: remove dead code (#96)
    This commit removes dead code in various places.
    CLI utilities removed will be replaced in the future.

--- dcs_simulation_engine/utils/paths.py
+++ dcs_simulation_engine/utils/paths.py
@@ -3,15 +3,10 @@
 from importlib import resources
 from pathlib import Path
 
-from dcs_simulation_engine.utils.package import (
-    get_package_name,
-)
-
 
 def package_root() -> Path:
     """Returns the root path of the dcs_simulation_engine package."""
-    pkg_name = get_package_name()
-    return Path(str(resources.files(pkg_name)))
+    return Path(str(resources.files("dcs_simulation_engine")))
 
 
 def package_games_dir() -> Path:
